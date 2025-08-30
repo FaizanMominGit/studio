b9e68b4b41d3850a27ff06486f09edcd3d85ed3a
Faizan <mdfaizanmomin12@gmail.com>
2025-08-30 16:36:43 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,21 +1,21 @@
+
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY, 
-  authDomain: "v-attendance-fcc06.firebaseapp.com",
-  projectId: "v-attendance-fcc06",
-  storageBucket: "v-attendance-fcc06.appspot.com",
-  messagingSenderId: "415391567793",
-  appId: "1:415391567793:web:36adc476ffb2374e703790",
-  measurementId: "G-2YP2T5LWMJ"
+  apiKey: "AIzaSyCqexkqHM0JE4Zqso_beFs0RT6_tgYjd9k",
+  authDomain: "attendabyte.firebaseapp.com",
+  projectId: "attendabyte",
+  storageBucket: "attendabyte.firebasestorage.app",
+  messagingSenderId: "310565587694",
+  appId: "1:310565587694:web:cd44b795b99dca2aa401ff",
+  measurementId: "G-N00RVW8XB6"
 };
 
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app); // Analytics is not used in this app
+// Analytics is not used in this app, so getAnalytics is not imported or called.
 export const auth = getAuth(app);
 export const db = getFirestore(app);
